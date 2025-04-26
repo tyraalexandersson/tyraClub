515847ba3c44a577e1ec397daf981f97f40e994c
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2025-04-26 21:25:03 +0200

    navbar and footer added

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.style.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="signature">
+        <p>Tyra Club</p>
+        <p>&copy; 2025</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
