2a244f32300000fcd8073c583e8c88f653ce1006
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2025-04-27 16:24:27 +0200

    adding logos and login and navbar hamb

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,36 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import { AppContextProvider, useAppContext } from "../context/contextProvider";
 
 function App() {
-  const { username, setUsername, routeHash } = useAppContext();
+  const { user, loading } = useAppContext(); // Using the context here
 
-  /* if (routeHash) {
-    if (routeHash.endsWith("&type=recovery")) {
-      window.location.replace(`/login/${routeHash}`);
-    }
-    if (routeHash.startsWith("#error_code=404"))
-      return (
-        <div>
-          <p>This link has expired</p>
-          <a href="/" style={{ cursor: "pointer" }} variant="link">
-            Back to app
-          </a>
-        </div>
-      );
-  } */
+  if (loading) return <div>Loading...</div>; // Show loading state while checking auth status
+
+  if (!user) return <Auth />; // Show login/signup form if user is not logged in
+
   return (
     <>
-      <AppContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </AppContextProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* You can add more protected routes here */}
+      </Routes>
+      <Footer />
     </>
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  );
+}
+
+export default AppWrapper;
