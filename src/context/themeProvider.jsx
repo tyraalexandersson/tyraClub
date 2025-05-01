5dc89d3f3b42f5a5d2fb4d4fb051cf714c3fd1ff
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2025-05-01 17:02:17 +0200

    added theme css body listeners

--- src/context/themeProvider.jsx
+++ src/context/themeProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const DEFAULT_THEME = "system";
 const initialState = {
@@ -14,6 +14,33 @@ export const ThemeContextProvider = ({ children }) => {
     theme,
     toggleTheme: (selectedTheme) => setTheme(selectedTheme),
   };
+
+  useEffect(() => {
+    const darkModePreference = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    );
+    if (theme === "system") {
+      darkModePreference.matches
+        ? document.body.classList.add("dark")
+        : document.body.classList.remove("dark");
+
+      const handleSystemThemeChange = (e) => {
+        if (e.matches) {
+          document.body.classList.add("dark");
+        } else {
+          document.body.classList.remove("dark");
+        }
+      };
+      darkModePreference.addEventListener("change", handleSystemThemeChange);
+    }
+
+    if (theme === "dark") {
+      document.body.classList.add("dark");
+    }
+    if (theme === "light") {
+      document.body.classList.remove("dark");
+    }
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
